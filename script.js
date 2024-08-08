59780a183a4de4897a42947e886adafc2548700e
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 11:33:00 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,35 @@
+console.log("Script running...")
+// Task 1: Hello button flow
+// Select the 'Click me for hello' button
+
+
+// Select the empty p tag under that button
+
+
+// Add an event listener to the hello button such that when it's click, the p tag text says "Hello"
+
+
+
+// Task 2: All caps button flow
+// Select the 'Click me for all caps' button
+
+
+// Select the p tag under that button (says there's no need to shout)
+
+
+// Add an event listener to the caps button such that when it's clicked, the p tag text is "THERE'S NO NEED TO SHOUT"
+
+
+
+// Task 3: Dog image flow
+// Select the dog image
+
+
+// Add an event listener to the dog image such that when it is clicked, the image src becomes happy-dog.jpeg
+
+
+
+
+
+
+
